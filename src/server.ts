482e880ff22a4eaa1f16811e70cca8b73f0c2aa2
Douglas <douglas.hsp2019@gmail.com>
2024-04-17 18:02:40 -0300

    applying changes

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,8 @@ app.post('/api/transcript', async (request, reply) => {
 })
 
 app.listen({
-  port: 3333,
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }).then(() => { 
   console.log('[INITIALIZED SERVER]')
 })
